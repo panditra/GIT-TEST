1ad8ba5ff7fa721f13a8429b2ae38cb5fa896086
panditra <rajpandit140@gmail.com>
2021-10-17 20:22:27 +0530

    Hi

---
+++ src/main/java/com/example/jpa/repository/CommentRepository.java
@@ -0,0 +1,19 @@
+package com.example.jpa.repository;
+
+import com.example.jpa.model.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+   // Page<Comment> findByPostId(Long postId, Pageable pageable);
+	List <Comment> findByPostId(Long PostId);
+    Optional<Comment> findByIdAndPostId(Long id, Long postId);
+	
+   // Comment findByPostId(Long Id);//practiceS
+}
